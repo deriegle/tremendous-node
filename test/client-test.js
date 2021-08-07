39c6f81782a05f6f8bfd2a5b4ffaa51acc33baec
Devin Riegle <riegledevin@gmail.com>
2021-08-07 11:37:39 -0400

    works

--- test/client-test.js
+++ test/client-test.js
@@ -1,6 +1,14 @@
 const TremendousClient = require('../lib/client');
 const fetchMock = require('fetch-mock');
 
+const getOrganizationsFixture = require('./fixtures/getOrganizations.json');
+const getProductsFixture = require('./fixtures/getProducts.json');
+const getOrdersFixture = require('./fixtures/getOrders.json');
+const getOrderFixture = require('./fixtures/getOrder.json');
+const getRewardFixture = require('./fixtures/getReward.json');
+const getFundingSourcesFixture = require('./fixtures/getFundingSources.json');
+const getFundingSourceFixture = require('./fixtures/getFundingSource.json');
+
 describe('TremendousClient', () => {
   test('it throws when not given an accessToken', () => {
     expect(() => new TremendousClient()).toThrowError(/Access token is required/i);
@@ -21,22 +29,7 @@ describe('TremendousClient', () => {
   });
 
   test('getOrganizations', async () => {
-    fetchMock.get('https://example.com/organizations', {
-      organizations: [
-        {
-          id: "ZQBSTARTSFSTW",
-          name: "Apple Inc",
-          website: "https://www.apple.com",
-          access_token: "[ACCESS_TOKEN]"
-        },
-        {
-          id: "QABSTARTSFSIO",
-          name: "Apple Inc - Sales Team",
-          website: "https://www.apple.com",
-          access_token: "[ACCESS_TOKEN]"
-        }
-      ]
-    });
+    fetchMock.get('https://example.com/organizations', getOrganizationsFixture);
 
     const client = new TremendousClient('1234', 'https://example.com');
     const res = await client.getOrganizations();
@@ -44,37 +37,11 @@ describe('TremendousClient', () => {
     expect(fetchMock.called('https://example.com/organizations')).toBe(true);
     expect(res.isSuccess).toBe(true);
     expect(res.isError).toBe(false);
-    expect(await res.json()).toEqual({
-      organizations: expect.any(Array),
-    });
+    expect(await res.json()).toEqual(getOrganizationsFixture);
   });
 
   test('getProducts', async () => {
-    fetchMock.get('https://example.com/products', {
-      products: [
-        {
-          id: "HX4U3DQX6GSA",
-          name:"adidas",
-          category: "merchant_card",
-          countries: [
-            {
-              abbr: "US"
-            },
-          ],
-          images: [
-            {
-              src: "https://s3.giftrocket.com/images/adidas"
-            }
-          ],
-          skus:[
-            {
-              "min":5,
-              "max":250
-            }
-          ]
-        }
-      ]
-    });
+    fetchMock.get('https://example.com/products', getProductsFixture);
 
     const client = new TremendousClient('1234', 'https://example.com');
     const res = await client.getProducts();
@@ -82,31 +49,11 @@ describe('TremendousClient', () => {
     expect(fetchMock.called('https://example.com/products')).toBe(true);
     expect(res.isSuccess).toBe(true);
     expect(res.isError).toBe(false);
-    expect(await res.json()).toEqual({
-      products: expect.any(Array),
-    });
+    expect(await res.json()).toEqual(getProductsFixture);
   });
 
   test('getOrders', async () => {
-    fetchMock.get('https://example.com/orders', {
-      orders: [
-        {
-          id: "QABSTARTSFSIO",
-          external_id: "[ID FROM CLIENT]",
-          status: "EXECUTED",
-          payment: {
-            "subtotal": 25.0,
-            "total": 25.5,
-            "fees": 0.5
-          },
-          rewards: [
-            {
-              id: "DDABSUKSFSTY"
-            }
-          ]
-        }
-      ]
-    });
+    fetchMock.get('https://example.com/orders', getOrdersFixture);
 
     const client = new TremendousClient('1234', 'https://example.com');
     const res = await client.getOrders();
@@ -114,30 +61,12 @@ describe('TremendousClient', () => {
     expect(fetchMock.called('https://example.com/orders')).toBe(true);
     expect(res.isSuccess).toBe(true);
     expect(res.isError).toBe(false);
-    expect(await res.json()).toEqual({
-      orders: expect.any(Array),
-    });
+    expect(await res.json()).toEqual(getOrdersFixture);
   });
 
   test('getOrder', async () => {
     const orderId = 'QABSTARTSFSIO';
-    fetchMock.get(`https://example.com/orders/${orderId}`, {
-      order: {
-        id: orderId,
-        external_id: "[ID FROM CLIENT]",
-        status: "CART",
-        payment: {
-          subtotal: 25.0,
-          total: 25.5,
-          fees: 0.5
-        },
-        rewards: [
-          {
-            id: "DDABSUKSFSTY"
-          }
-        ]
-      }
-    });
+    fetchMock.get(`https://example.com/orders/${orderId}`, getOrderFixture);
 
     const client = new TremendousClient('1234', 'https://example.com');
     const res = await client.getOrder(orderId);
@@ -145,33 +74,12 @@ describe('TremendousClient', () => {
     expect(fetchMock.called(`https://example.com/orders/${orderId}`)).toBe(true);
     expect(res.isSuccess).toBe(true);
     expect(res.isError).toBe(false);
-    expect(await res.json()).toEqual({
-      order: expect.objectContaining({
-        id: orderId,
-      })
-    });
+    expect(await res.json()).toEqual(getOrderFixture);
   });
 
   test('getReward', async () => {
     const rewardId = 'QABSTARTSFSIO';
-    fetchMock.get(`https://example.com/rewards/${rewardId}`, {
-      reward: {
-        id: rewardId,
-        value: {
-          denomination: 30,
-          currency_code: "USD"
-        },
-        recipient: {
-          name: "Denise Miller",
-          email: "denise@sales.com",
-        },
-        products: ["HX4U3DQX6GSA"],
-        delivery: {
-          method: "EMAIL",
-          status: "SUCCEEDED"
-        }
-      }
-    });
+    fetchMock.get(`https://example.com/rewards/${rewardId}`, getRewardFixture);
 
     const client = new TremendousClient('1234', 'https://example.com');
     const res = await client.getReward(rewardId);
@@ -179,35 +87,11 @@ describe('TremendousClient', () => {
     expect(fetchMock.called(`https://example.com/rewards/${rewardId}`)).toBe(true);
     expect(res.isSuccess).toBe(true);
     expect(res.isError).toBe(false);
-    expect(await res.json()).toEqual({
-      reward: expect.objectContaining({
-        id: rewardId,
-      })
-    });
+    expect(await res.json()).toEqual(getRewardFixture);
   });
 
   test('getFundingSources', async () => {
-    fetchMock.get('https://example.com/funding_sources', {
-      funding_sources: [
-        {
-          id: "GCTHLCMFVYMD",
-          method: "balance",
-          meta: {
-            available_cents: 500000,
-            pending_cents: 0
-          }
-        },
-        {
-          id: "LARFAF2423",
-          method: "ach",
-          meta: {
-            accountholder_name: "James Bay",
-            account_number_mask: "6789",
-            bank_name: "Chase"
-          }
-        }
-      ]
-    });
+    fetchMock.get('https://example.com/funding_sources', getFundingSourcesFixture);
 
     const client = new TremendousClient('1234', 'https://example.com');
     const res = await client.getFundingSources();
@@ -215,24 +99,12 @@ describe('TremendousClient', () => {
     expect(fetchMock.called('https://example.com/funding_sources')).toBe(true);
     expect(res.isSuccess).toBe(true);
     expect(res.isError).toBe(false);
-    expect(await res.json()).toEqual({
-      funding_sources: expect.any(Array),
-    });
+    expect(await res.json()).toEqual(getFundingSourcesFixture);
   });
 
   test('getFundingSource', async () => {
     const fundingSourceId = 'LARFAF2423';
-    fetchMock.get(`https://example.com/funding_sources/${fundingSourceId}`, {
-      funding_source: {
-        id: "LARFAF2423",
-        method: "ach",
-        meta: {
-          accountholder_name: "James Bay",
-          account_number_mask: "6789",
-          bank_name: "Chase"
-        }
-      }
-    });
+    fetchMock.get(`https://example.com/funding_sources/${fundingSourceId}`, getFundingSourceFixture);
 
     const client = new TremendousClient('1234', 'https://example.com');
     const res = await client.getFundingSource(fundingSourceId);
@@ -240,10 +112,6 @@ describe('TremendousClient', () => {
     expect(fetchMock.called(`https://example.com/funding_sources/${fundingSourceId}`)).toBe(true);
     expect(res.isSuccess).toBe(true);
     expect(res.isError).toBe(false);
-    expect(await res.json()).toEqual({
-      funding_source: expect.objectContaining({
-        id: fundingSourceId,
-      })
-    });
+    expect(await res.json()).toEqual(getFundingSourceFixture);
   });
 });
